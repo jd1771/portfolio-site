cbe2190af07e284d80e1eee939120be9a20d449f
Jas Dhaul <jasdhaul0@gmail.com>
2024-12-28 11:51:50 -0800

    Update headers titles again.

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
       <h1>Jaskaran Dhaul</h1>
       <nav>
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
+          <li><a href="#home">About</a></li>
+          <li><a href="#about">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
