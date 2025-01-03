948f27c6b3fc9bd429fc3e4d213645d604c0bbd8
Jas Dhaul <jasdhaul0@gmail.com>
2025-01-03 14:06:17 -0800

    Add AboutMe CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header.jsx'
+import AboutMe from './components/AboutMe/AboutMe.jsx'
 
 function App() {
   return (
     <> 
       <Header />
+      <AboutMe />
     </>
   )
 }
--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './AboutMe.css';
 
-const AboutMe: React.FC = () => {
+const AboutMe = () => {
+  
   return (
     <section className="about-me">
       <div className="container">
-        <h1 className="intro-text">Hello, I'm John Doe</h1>
+        <h1 className="intro-text">Hello, I'm Jas</h1>
         <h2 className="subtitle">I'm a Software Developer</h2>
         <p className="scroll-message">Scroll below to learn more about me!</p>
       </div>
     </section>
   );
+
 };
 
 export default AboutMe;
