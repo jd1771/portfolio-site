74fbb14346ac5cc82ea25602b2aedc9d3d3fa4b9
Jas Dhaul <jasdhaul0@gmail.com>
2025-01-02 20:53:37 -0800

    Organize codebase.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
-import Header from './Header'
+import Header from './components/Header/Header.jsx'
 
 function App() {
   return (
     <> 
       <Header />
-     
     </>
   )
 }
