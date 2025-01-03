77f881ee40a1902a6e8e24c754eb0f049392bfd1
Jas Dhaul <jasdhaul0@gmail.com>
2025-01-02 21:07:20 -0800

    Added mobile menu.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'; 
 
 const Header = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="header-content">
         <h1 className="logo">Jaskaran Dhaul</h1>
-        <nav>
+        <button className="menu-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <nav className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
           <ul className="nav-links">
-            <li><a href="#about">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about" onClick={() => setIsMenuOpen(false)}>About</a></li>
+            <li><a href="#projects" onClick={() => setIsMenuOpen(false)}>Projects</a></li>
+            <li><a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a></li>
           </ul>
         </nav>
       </div>
