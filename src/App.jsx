3c2cb8a14ffb2071252cdff53764f0ff8dc5ab44
Jas Dhaul <jasdhaul0@gmail.com>
2025-01-04 13:38:09 -0800

    Create experience jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header/Header.jsx'
 import AboutMe from './components/AboutMe/AboutMe.jsx'
+import Experience from './components/Experience/Experience.jsx'
 
 function App() {
   return (
     <> 
       <Header />
       <AboutMe />
+      <Experience />
     </>
   )
 }
