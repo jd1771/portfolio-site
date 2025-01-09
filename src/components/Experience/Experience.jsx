40a08c2a9c6144b27bb6967a8cb59bea014a6fd3
Jas Dhaul <jasdhaul0@gmail.com>
2025-01-09 15:01:50 -0800

    More indent fixes.

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -4,22 +4,22 @@ import './Experience.css';
 
 const experienceData = [
   {
-      title: "Software Developer Intern",
-      company: "STN Video",
-      period: "Sept 2023 - Apr 2024",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis libero sit amet enim fringilla scelerisque"
+    title: "Software Developer Intern",
+    company: "STN Video",
+    period: "Sept 2023 - Apr 2024",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis libero sit amet enim fringilla scelerisque"
   },
   {
-      title: "Programmer Analyst Intern",
-      company: "BC Public Service",
-      period: "Jan 2023 - Apr 2023",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis libero sit amet enim fringilla scelerisque"
+    title: "Programmer Analyst Intern",
+    company: "BC Public Service",
+    period: "Jan 2023 - Apr 2023",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis libero sit amet enim fringilla scelerisque"
   },
   {
-      title: "Programmer Analyst Intern",
-      company: "BC Public Service",
-      period: "May 2022 - Aug 2022",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis libero sit amet enim fringilla scelerisque"
+    title: "Programmer Analyst Intern",
+    company: "BC Public Service",
+    period: "May 2022 - Aug 2022",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis libero sit amet enim fringilla scelerisque"
   }
 ]
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,31 +4,31 @@ import './Projects.css';
 
 const projectData = [
   {
-      id: 1,
-      title: "Portfolio Website",
-      description: "A responsive portfolio website built with React and Next.js",
-      githubUrl: "https://github.com/",
-      liveUrl: "https://www.abc.com"
+    id: 1,
+    title: "Portfolio Website",
+    description: "A responsive portfolio website built with React and Next.js",
+    githubUrl: "https://github.com/",
+    liveUrl: "https://www.abc.com"
   },
   {
-      id: 2,
-      title: "Portfolio Website",
-      description: "A responsive portfolio website built with React and Next.js",
-      githubUrl: "https://github.com/",
-      liveUrl: "https://www.abc.com"
+    id: 2,
+    title: "Portfolio Website",
+    description: "A responsive portfolio website built with React and Next.js",
+    githubUrl: "https://github.com/",
+    liveUrl: "https://www.abc.com"
   },
   {
-      id: 3,
-      title: "Portfolio Website",
-      description: "A responsive portfolio website built with React and Next.js",
-      githubUrl: "https://github.com/",
-      liveUrl: "https://www.abc.com"
+    id: 3,
+    title: "Portfolio Website",
+    description: "A responsive portfolio website built with React and Next.js",
+    githubUrl: "https://github.com/",
+    liveUrl: "https://www.abc.com"
   },
   {
-      id: 4,
-      title: "Portfolio Website",
-      description: "A responsive portfolio website built with React and Next.js",
-      githubUrl: "https://github.com/",
+    id: 4,
+    title: "Portfolio Website",
+    description: "A responsive portfolio website built with React and Next.js",
+    githubUrl: "https://github.com/",
   }
 ]
 
